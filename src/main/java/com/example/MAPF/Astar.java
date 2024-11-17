c4c43808629d8be7949e79a713a32f4b7e47de96
Derek <derekzongnanchen@gmail.com>
2024-11-16 21:10:23 -0600

    feat: astar

--- src/main/java/com/example/MAPF/Astar.java
+++ src/main/java/com/example/MAPF/Astar.java
@@ -2,6 +2,39 @@ package com.example.MAPF;
 
 import java.util.*;
 
+class Node {
+    private int[] pos;
+    private int[] pPos;
+    private int g;
+    private int h;
+    private int condition;
+    public Node( int[] pos, int[] pPos, int g, int h, int condition) {
+        this.pos = pos;
+        this.pPos = pPos;
+        this.g = g;
+        this.h = h;
+        this.condition = condition;
+    }
+
+    public int[] getPos() {
+        return this.pos;
+    }
+
+    public int[] getParentPos() {
+        return this.pPos;
+    }
+
+    public int getF() {return this.g+this.f;}
+    public int getG() {return this.g;}
+    public int getH() {return this.h;}
+    public int getCondition() {return this.condition};
+
+    public void setG(int g) {this.g = g;}
+    public void setH(int h) {this.h = h;}
+
+
+}
+
 public class Astar {
     static List<int[]> direction = Arrays.asList(
             new int[]{0,1},
@@ -10,38 +43,52 @@ public class Astar {
             new int[]{-1,0}
     );
     public static List<int[]> AstarAlgo(int[] originNode, int[] destNode, int[][] grid) {
-        int[] curNode = originNode;
-        while (!Arrays.equals(curNode, destNode)) {
-            List<int[]> neighbors = findNeighbor(curNode, grid);
-            int[] maxDist = {Integer.MAX_VALUE, Integer.MAX_VALUE};
-            for (int[] neighbor: neighbors) {
-                List<Integer> dist = CalculateDistance.findDist(curNode,destNode,originNode);
-                if (dist.stream().map<maxDist[0]+maxDist[1]) {
+        // do a priority queue where the top is the smallest F. 
+        // if a node is visited we will get its preV value and compare with current
+        // Nodes carry parent info and cost
+        // Div the grid into nodes
+        // Node contain[curPos, ParentPos, G-cost, H-cost, F-cost]
 
-                }
+        // initialize all points in the grid to be nodes
+        Node[][] gridN = new Node[grid.length][grid[0].length];
+        for (int i = 0; i<gridN.length; i++) {
+            for (int j = 0; j<gridN[0].length;j++) {
+                gridN[i][j] = new Node(new int[]{i,j}, new int[2], Integer.MAX_VALUE, CalculateDistance.findDist(new int[]{i,j}, destNode),grid[i][j]);
             }
+        }
+        gridN[originNode[0]][originNode[1]].setG(0);
+        // priority queue
+        PriorityQueue<Node> pq = new PriorityQueue<>(Comparator.comparingInt((Node node)->node.getF()));
+        pq.offer(gridN[originNode[0]][originNode[1]]);
+        while (!pq.isEmpty()) {
+            Node curNode = pq.poll();
+            int curF = curNode.getF();
+            int curG = curNode.getG();
+            int curH = curNode.getH();
 
-
+            for (Node node: findNeighbor(curNode, gridN)) {
+                if
+            }
         }
 
         return new ArrayList<>();
     }
 
-    private static List<int[]> findNeighbor(int[] node, int[][] grid) {
-        List<int[]> result = new ArrayList<>();
+    private static List<Node> findNeighbor(Node node, Node[][] gridN) {
+        List<Node> result = new ArrayList<>();
         for (int[] d: direction) {
-            int newRow = node[0]+d[0];
-            int newCol = node[1]+node[1];
+            int newRow = node.getPos()[0]+d[0];
+            int newCol = node.getPos()[1]+d[1];
 
-            if(!isOutOfBound(grid,newRow,newCol)&&grid[newRow][newCol]==0) {
-                result.add(new int[]{newRow, newCol});
+            if(!isOutOfBound(gridN,newRow,newCol)&&gridN[newRow][newCol].getCondition()==0) {
+                result.add(gridN[newRow][newCol]);
             }
         }
         return result;
     }
-    private static boolean isOutOfBound(int[][] grid, int row, int col) {
-        int maxRow = grid.length;
-        int maxCol = grid[0].length;
+    private static boolean isOutOfBound(Node[][] gridN, int row, int col) {
+        int maxRow = gridN.length;
+        int maxCol = gridN[0].length;
         return (row<maxRow&&col<maxCol&&row>=0&&col>=0);
     }
 }
